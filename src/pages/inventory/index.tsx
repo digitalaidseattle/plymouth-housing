21e8a4f3c56bb26590f2755af5b75d027403c92b
aitzeng <aitzeng@gmail.com>
2024-09-12 16:53:16 -0700

    Cleanup unused imports

--- src/pages/inventory/index.tsx
+++ src/pages/inventory/index.tsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import dummyData from './data.js';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';

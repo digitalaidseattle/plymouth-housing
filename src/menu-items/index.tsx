1e086fd626ff908305e710d129e2999cd929fcda
Maarten van de Bospoort <maarten.van-de-bospoort@digitalaidseattle.org>
2024-09-22 18:51:01 +0000

    cleanup to minimize build size

--- src/menu-items/index.tsx
+++ src/menu-items/index.tsx
@@ -44,22 +44,6 @@ const dashboard = {
       breadcrumbs: false,
     },
     {
-      id: 'tickets',
-      title: 'Tickets',
-      type: 'item',
-      url: '/tickets',
-      icon: icons.FileOutlined,
-      breadcrumbs: false,
-    },
-    {
-      id: 'tickets-grid',
-      title: 'Advanced Tickets Table',
-      type: 'item',
-      url: '/tickets-grid',
-      icon: icons.FileExclamationOutlined,
-      breadcrumbs: false,
-    },
-    {
       id: 'inventory',
       title: 'Inventory',
       type: 'item',
@@ -70,69 +54,9 @@ const dashboard = {
   ],
 };
 
-const pages = {
-  id: 'example',
-  title: 'Examples',
-  type: 'group',
-  children: [
-    {
-      id: 'login1',
-      title: 'Login',
-      type: 'item',
-      url: '/login',
-      icon: icons.LoginOutlined,
-    },
-    {
-      id: '404',
-      title: '404',
-      type: 'item',
-      url: '/404',
-      icon: icons.ExclamationOutlined,
-    },
-    {
-      id: 'sample-page',
-      title: 'Sample Page',
-      type: 'item',
-      url: '/sample-page',
-      icon: icons.ChromeOutlined,
-    },
-    {
-      id: 'privacy-page',
-      title: 'Privacy Page',
-      type: 'item',
-      url: '/privacy',
-      icon: icons.EyeInvisibleOutlined,
-      breadcrumbs: false,
-    },
-    {
-      id: 'upload-page',
-      title: 'Upload Page',
-      type: 'item',
-      url: '/upload',
-      icon: icons.UploadOutlined,
-      breadcrumbs: false,
-    },
-    {
-      id: 'drag-drop-page',
-      title: 'Drag Drop Page',
-      type: 'item',
-      url: '/drag-drop',
-      icon: icons.DragOutlined,
-      breadcrumbs: false,
-    },
-    {
-      id: 'map-example-page',
-      title: 'Map Example Page',
-      type: 'item',
-      url: '/map-example',
-      icon: icons.GlobalOutlined,
-      breadcrumbs: false,
-    },
-  ],
-};
 
 const menuItems = {
-  items: [dashboard, pages],
+  items: [dashboard],
 };
 
 export default menuItems;

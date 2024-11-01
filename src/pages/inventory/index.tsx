24f21fe5506706012d00071cb44e50ab2be103d0
aitzeng <aitzeng@gmail.com>
2024-10-31 17:02:31 -0700

    Created modal

--- src/pages/inventory/index.tsx
+++ src/pages/inventory/index.tsx
@@ -8,7 +8,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
-import { Button, Menu, Pagination, Typography } from '@mui/material';
+import { Button, Menu, Modal, Pagination, Select, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ClearIcon from '@mui/icons-material/Clear';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
@@ -26,13 +26,15 @@ type InventoryItem = {
   status: string;
 };
 
-const API = "http://localhost:4280/data-api/rest/item";
+const API = "/data-api/rest/item";
 const HEADERS = { 'Accept': 'application/json', 'Content-Type': 'application/json;charset=utf-8' };
 
 const Inventory = () => {
   const [originalData, setOriginalData] = useState<InventoryItem[]>([]);
   const [displayData, setDisplayData] = useState<InventoryItem[]>([]);
   const [itemAlph, setItemAlph] = useState<'asc' | 'desc' | 'original'>('original');
+  const [addModal, setAddModal] = useState(false);
+  const [addItemType, setAddItemType] = useState('');
   const [type, setType] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [category, setCategory] = useState('');
@@ -59,6 +61,17 @@ const Inventory = () => {
   const handleStatusClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorStatus(event.currentTarget);
   };
+  const handleAddClick = () => {
+    setAddModal(true)
+  }
+
+  const handleAddClose = () => {
+    setAddModal(false)
+  }
+
+  const handleAddItemType = (event: { target: { value: string } }) => {
+    setAddItemType(event.target.value)
+  }
 
   const itemAlphabetizeHandle = () => {
     if (itemAlph === 'asc') {
@@ -212,10 +225,47 @@ const Inventory = () => {
     <Box>
       {/* Add button */}
       <Box id="add-container" sx={{ display: 'flex', justifyContent: 'end' }}>
-        <Button sx={{ bgcolor: '#F5F5F5', color: 'black' }}>
+        <Button sx={{ bgcolor: '#F5F5F5', color: 'black' }} onClick={handleAddClick}>
           <AddIcon fontSize="small" sx={{ color: 'black' }} />
           Add
         </Button>
+        <Modal
+          open={addModal}
+          onClose={handleAddClose}
+        >
+          <Box sx={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '50%', height: '50%', backgroundColor: 'white' }}>
+            <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly', width: '80%', margin: 'auto', height: '100%'}}>
+              <Typography>
+                Add Item
+              </Typography>
+              <Box id="add-item-type">
+                <Typography>
+                  Item Type
+                </Typography>
+                <Select
+                  value={addItemType}
+                  onChange={handleAddItemType}
+                  sx={{ minWidth: '200px' }}
+                >
+                  <MenuItem value={'Donation'}>Donation</MenuItem>
+                  <MenuItem value={'Welcome Basket'}>Welcome Basket</MenuItem>
+                </Select>
+              </Box>
+              <Box id="add-item-name">
+                <Typography>
+                  Item
+                </Typography>
+                <TextField></TextField>
+              </Box>
+              <Box id="add-item-quantity">
+                <Typography>
+                  Quantity
+                </Typography>
+                <TextField></TextField>
+              </Box>
+            </Box>
+          </Box>
+        </Modal>
       </Box>
 
       {/* Filter Container */}
@@ -389,14 +439,14 @@ const Inventory = () => {
               <TableRow>
                 <TableCell sx={{ fontWeight: 'bold', cursor: 'pointer', display: 'flex', alignItems: 'center' }} onClick={itemAlphabetizeHandle}>Name
                   {itemAlph === 'asc' ? (
-                  <ArrowUpwardIcon fontSize="small" sx={{fontWeight: 'normal', ml: 0.5, color: 'gray'}}/>
-                ) : itemAlph === 'desc' ? (
-                  <ArrowDownwardIcon fontSize="small" sx={{fontWeight: 'normal', ml: 0.5, color: 'gray'}}/>
-                ) : null}</TableCell>
-                <TableCell>Type</TableCell>
-                <TableCell>Category</TableCell>
-                <TableCell>Status</TableCell>
-                <TableCell>Quantity</TableCell>
+                    <ArrowUpwardIcon fontSize="small" sx={{ fontWeight: 'normal', ml: 0.5, color: 'gray' }} />
+                  ) : itemAlph === 'desc' ? (
+                    <ArrowDownwardIcon fontSize="small" sx={{ fontWeight: 'normal', ml: 0.5, color: 'gray' }} />
+                  ) : null}</TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}>Type</TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}>Category</TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}>Status</TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}>Quantity</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>

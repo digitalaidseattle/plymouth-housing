dc4b72aab77ad827801115ce830ae409c4f9a75f
Maarten van de Bospoort <bospoor@live.com>
2024-11-17 17:53:22 -0800

    adding user

--- src/pages/authentication/PickNamePage.tsx
+++ src/pages/authentication/PickNamePage.tsx
@@ -11,7 +11,10 @@ import MinimalWrapper from '../../layout/MinimalLayout/MinimalWrapper';
 import CenteredLayout from './CenteredLayout';
 import SnackbarAlert from './SnackbarAlert';
 import { getRole, UserContext } from '../../components/contexts/UserContext';
-import {HEADERS} from '../../types/constants'
+import { HEADERS } from '../../types/constants'
+import { IdTokenClaims } from '@azure/msal-common';
+import { useMsal } from '@azure/msal-react';
+
 
 const API = '/data-api/rest/volunteer';
 
@@ -26,13 +29,36 @@ const PickYourNamePage: React.FC = () => {
   );
   const [volunteers, setVolunteers] = useState<Volunteer[]>([]);
   const [openSnackbar, setOpenSnackbar] = useState<boolean>(false);
-  const {user} = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
+//  const [user, setUser] = useState<IdTokenClaims | null>(null);
+  const { instance } = useMsal();
 
   useEffect(() => {
+    const account = instance.getActiveAccount();
+    if (account) {
+      instance
+        .acquireTokenSilent({
+          account: account,
+          scopes: ['openid', 'profile', 'email', 'User.Read'],
+        })
+        .then((response) => {
+          const userClaims = response.idTokenClaims;
+          setUser(userClaims || null);
+        })
+        .catch((error) => {
+          console.error('Error acquiring token', error);
+        });
+    }
+  }, [instance, setUser]);
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  useEffect(() => {
+    if (!user) return;
     const fetchVolunteers = async () => {
       try {
-        console.log('roles', user?.roles);
         HEADERS['X-MS-API-ROLE'] = getRole(user);
         const response = await fetch(
           `${API}?$select=id,name&$filter=active eq true`,
@@ -41,20 +67,17 @@ const PickYourNamePage: React.FC = () => {
             headers: HEADERS,
           },
         );
-
         if (!response.ok) {
           throw new Error(`Error: ${response.statusText}`);
         }
         const data = await response.json();
-
         setVolunteers(data.value);
       } catch (error) {
         console.error('Failed to fetch volunteers:', error);
       }
     };
-
     fetchVolunteers();
-  }, []);
+  }, [user]);
 
   const handleNameChange = (
     _event: React.SyntheticEvent,
@@ -85,81 +108,84 @@ const PickYourNamePage: React.FC = () => {
   };
 
   return (
-    <MinimalWrapper>
-      <CenteredLayout>
-        <Box sx={{ maxWidth: '350px', width: '100%' }}>
-          <Typography
-            variant="h4"
-            textAlign="left"
-            sx={{
-              height: '50px',
-              lineHeight: '50px',
-              marginBottom: 2,
-            }}
+    <UserContext.Provider value={{ user, setUser }}>
+      <MinimalWrapper>
+        <CenteredLayout>
+          <Box sx={{ maxWidth: '350px', width: '100%' }}>
+            <Typography
+              variant="h4"
+              textAlign="left"
+              sx={{
+                height: '50px',
+                lineHeight: '50px',
+                marginBottom: 2,
+              }}
+            >
+              Pick Your Name
+            </Typography>
+
+            <Typography
+              variant="body2"
+              sx={{
+                maxWidth: '100%',
+                textAlign: 'left',
+                marginBottom: 4,
+                lineHeight: 1.5,
+              }}
+            >
+              <strong>Can't find your name?</strong> Let a staff member know or
+              contact IT department at {import.meta.env.VITE_ADMIN_PHONE_NUMBER}
+            </Typography>
+
+            <Autocomplete
+              value={selectedVolunteer}
+              onChange={handleNameChange}
+              options={volunteers}
+              getOptionLabel={(option) => option.name}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Select your name"
+                  variant="outlined"
+                  sx={{ width: '100%' }}
+                />
+              )}
+              sx={{
+                width: '100%',
+                marginBottom: 8,
+                '& .MuiAutocomplete-inputRoot': { height: '56px' },
+              }}
+            />
+
+            <Button
+              variant="contained"
+              onClick={handleNextClick}
+              sx={{
+                height: '45px',
+                width: '100%',
+                fontSize: '16px',
+                backgroundColor: 'black',
+                color: 'white',
+                '&:hover': {
+                  backgroundColor: '#4f4f4f',
+                },
+              }}
+            >
+              Continue
+            </Button>
+          </Box>
+
+          <SnackbarAlert
+            open={openSnackbar}
+            onClose={handleSnackbarClose}
+            severity="warning"
           >
-            Pick Your Name
-          </Typography>
+            Please select a name before continuing.
+          </SnackbarAlert>
+        </CenteredLayout>
+      </MinimalWrapper>
+    </UserContext.Provider>
 
-          <Typography
-            variant="body2"
-            sx={{
-              maxWidth: '100%',
-              textAlign: 'left',
-              marginBottom: 4,
-              lineHeight: 1.5,
-            }}
-          >
-            <strong>Can't find your name?</strong> Let a staff member know or
-            contact IT department at {import.meta.env.VITE_ADMIN_PHONE_NUMBER}
-          </Typography>
-
-          <Autocomplete
-            value={selectedVolunteer}
-            onChange={handleNameChange}
-            options={volunteers}
-            getOptionLabel={(option) => option.name}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Select your name"
-                variant="outlined"
-                sx={{ width: '100%' }}
-              />
-            )}
-            sx={{
-              width: '100%',
-              marginBottom: 8,
-              '& .MuiAutocomplete-inputRoot': { height: '56px' },
-            }}
-          />
-
-          <Button
-            variant="contained"
-            onClick={handleNextClick}
-            sx={{
-              height: '45px',
-              width: '100%',
-              fontSize: '16px',
-              backgroundColor: 'black',
-              color: 'white',
-              '&:hover': {
-                backgroundColor: '#4f4f4f',
-              },
-            }}
-          >
-            Continue
-          </Button>
-        </Box>
-
-        <SnackbarAlert
-          open={openSnackbar}
-          onClose={handleSnackbarClose}
-          severity="warning"
-        >
-          Please select a name before continuing.
-        </SnackbarAlert>
-      </CenteredLayout>
-    </MinimalWrapper>
   );
 };
 

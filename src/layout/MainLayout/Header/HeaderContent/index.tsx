52a1843d36759a5a521746f39e0c5d08cea16985
jingtechy <jingtechy@gmail.com>
2024-11-26 19:48:16 +1300

    Add comments

--- src/layout/MainLayout/Header/HeaderContent/index.tsx
+++ src/layout/MainLayout/Header/HeaderContent/index.tsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 // material-ui
 import { Box, useMediaQuery, useTheme } from '@mui/material';
 
-// project import
+// project import, it's for post-MVP so we comment it out for now
 // import toolbarItems from '../../../../toolbar-items';
 import MobileSection from './MobileSection';
 import Profile from './Profile';
@@ -20,6 +20,7 @@ const HeaderContent = () => {
     <>
       {matchesXs && <Box sx={{ width: '100%', ml: 1 }} />}
       {user?.roles?.includes('volunteer') && <VolunteerSwitcher />}
+      {/* We hide the notification bell for now as it's for post-MVP*/}
       {/* {toolbarItems.items} */}
       <Box sx={{ flexGrow: 1}}  />
       {!matchesXs && <Profile />}

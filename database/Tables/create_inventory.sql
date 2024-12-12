8697f274a0df799d9742e66994707c0883254eb4
Maarten van de Bospoort <bospoor@live.com>
2024-12-11 16:33:11 -0800

    prepare for checkout changes

--- database/Tables/create_inventory.sql
+++ database/Tables/create_inventory.sql
@@ -10,5 +10,40 @@ CREATE TABLE Items (
     low INT NOT NULL,
     medium INT NOT NULL,
 );
+GO
 
+CREATE VIEW InventoryWithCategory 
+AS 
+    SELECT 
+        i.id, 
+        i.name, 
+        i.type, 
+        c.name AS category, 
+        i.description, 
+        i.quantity, 
+        i.status 
+    FROM 
+        Items i
+    LEFT JOIN 
+        Categories c ON c.id = i.category_id;
+GO
+
+CREATE VIEW ItemsByCategory
+AS
+    SELECT
+        Categories.id,
+        Categories.name AS category,
+        (
+        SELECT
+            Items.id,
+            Items.name,
+            Items.quantity
+        FROM
+            Items
+        WHERE
+            Items.category_id = Categories.id
+        FOR JSON PATH
+    ) AS items
+    FROM
+        Categories;
 GO

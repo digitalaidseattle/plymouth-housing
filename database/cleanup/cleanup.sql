56049ea7339afff1403c1fb2ca0acc1c9e55b6e9
Maarten van de Bospoort <bospoor@live.com>
2025-01-08 16:41:06 -0800

    removing race condition

--- database/cleanup/cleanup.sql
+++ database/cleanup/cleanup.sql
@@ -4,7 +4,7 @@ DROP VIEW IF EXISTS ItemsByCategory;
 DROP PROCEDURE IF EXISTS ProcessCheckout;
 DROP PROCEDURE IF EXISTS CheckCartItemLimit;
 DROP PROCEDURE IF EXISTS CheckCategoryCheckoutLimit;
-DROP PROCEDURE IF EXISTS CartItemsType;
+DROP PROCEDURE IF EXISTS CheckInsufficientInventory;
 
 DROP TYPE IF EXISTS CheckCartItemLimit;
 DROP TYPE IF EXISTS CartItemsType;
@@ -12,3 +12,5 @@ DROP TYPE IF EXISTS CartItemsType;
 DROP TABLE IF EXISTS Transactions; -- Has a Foreign Key constraint on Items.
 DROP TABLE IF EXISTS Items;
 DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Buildings;
+DROP TABLE IF EXISTS Volunteers;

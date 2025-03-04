b772b5d910efd430fc169ee85bf45416d056c856
Misty <mistyb140@gmail.com>
2025-03-04 13:51:08 -0800

    define endpoint constant

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -14,6 +14,7 @@ export const ENDPOINTS = {
   VERIFY_PIN: '/data-api/rest/verify-pin',
   CHECKOUT_GENERAL_ITEMS: '/data-api/rest/checkout-general-items',
   CHECKOUT_WELCOME_BASKET: '/data-api/rest/checkout-welcome-basket',
+  RECENT_TRANSACTIONS: '/data-api/rest/recent-transactions',
   //Views
   EXPANDED_ITEMS: '/data-api/rest/itemswithcategory',
   CATEGORIZED_ITEMS: '/data-api/rest/itemsbycategory',

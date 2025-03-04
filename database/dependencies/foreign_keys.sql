b110f91aa52d8b0ece4e572be43eb84f381e1763
Misty <mistyb140@gmail.com>
2025-03-04 13:09:20 -0800

    alter table statements

--- database/dependencies/foreign_keys.sql
+++ database/dependencies/foreign_keys.sql
@@ -3,3 +3,13 @@
 ALTER TABLE Transactions
 ADD CONSTRAINT FK_Transactions_Items
 FOREIGN KEY (item_id) REFERENCES Items(id);
+
+-- Add boolean value to Items to determine if it requires tracking
+ALTER TABLE Items
+ADD requires_tracking BIT NOT NULL DEFAULT 0;
+
+-- Add unit number, resident name, and additional notes to Transactions
+ALTER TABLE Transactions
+ADD unit_number VARCHAR(10),
+resident_name VARCHAR(50),
+additional_notes TEXT;

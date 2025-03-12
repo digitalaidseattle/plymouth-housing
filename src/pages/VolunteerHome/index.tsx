5a64c0faf9e207035c37c6b807f96a272c282b3a
Maarten van de Bospoort <bospoor@live.com>
2025-03-11 17:42:18 -0700

    fix lint error

--- src/pages/VolunteerHome/index.tsx
+++ src/pages/VolunteerHome/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect, useCallback } from 'react';
+import React, { useState, useContext, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box, Typography, Button, Grid } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';

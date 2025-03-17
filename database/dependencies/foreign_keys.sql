6025513aa95f4494d92e2cbcac68d04eb6b52d46
Misty <mistyb140@gmail.com>
2025-03-16 21:18:49 -0700

    create tracking table

--- database/dependencies/foreign_keys.sql
+++ database/dependencies/foreign_keys.sql
@@ -8,3 +8,7 @@ FOREIGN KEY (item_id) REFERENCES dbo.Items(id);
 ALTER TABLE dbo.TransactionItems
 ADD CONSTRAINT FK_TransactionItems_TransactionId
 FOREIGN KEY (transaction_id) REFERENCES dbo.Transactions(id);
+
+ALTER TABLE dbo.Tracking
+ADD CONSTRAINT FK_Tracking_ItemId
+FOREIGN KEY (item_id) REFERENCES dbo.Items(id);

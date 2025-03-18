81c28f3041e37631234d8d2061cef8b1a28d650a
Maarten van de Bospoort <bospoor@live.com>
2025-03-18 15:27:45 -0700

    completing cleanup script

--- database/cleanup/cleanup.sql
+++ database/cleanup/cleanup.sql
@@ -2,17 +2,22 @@ DROP VIEW IF EXISTS ItemsWithCategory;
 DROP VIEW IF EXISTS ItemsByCategory;
 
 DROP PROCEDURE IF EXISTS ProcessCheckout;
+DROP PROCEDURE IF EXISTS ProcessWelcomeBasketCheckout;
 DROP PROCEDURE IF EXISTS CheckCategoryCheckoutLimit;
-DROP PROCEDURE IF EXISTS CheckInsufficientInventory
+DROP PROCEDURE IF EXISTS CheckInsufficientInventory;
 DROP PROCEDURE IF EXISTS CheckCartItemLimit;
+DROP PROCEDURE IF EXISTS LogTransaction;
+DROP PROCEDURE IF EXISTS LogTransactionItem;
+DROP PROCEDURE IF EXISTS GetRecentTransactions;
+DROP PROCEDURE IF EXISTS VerifyVolunteerPin;
 
 DROP TYPE IF EXISTS CartItemsType;
 
-DROP TABLE IF EXISTS TransactionItems; -- Has a Foreign Key constraint on Items and Transactions
-DROP TABLE IF EXISTS TransactionTypes; 
-DROP TABLE IF EXISTS Tracking; -- Has a Foreign Key constraint on Items
-DROP TABLE IF EXISTS Transactions; 
+DROP TABLE IF EXISTS TransactionItems; 
+DROP TABLE IF EXISTS TransactionTypes;
+DROP TABLE IF EXISTS Tracking; 
+DROP TABLE IF EXISTS Transactions;
 DROP TABLE IF EXISTS Items;
 DROP TABLE IF EXISTS Categories;
-DROP TABLE IF EXISTS Volunteers;
 DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Buildings;

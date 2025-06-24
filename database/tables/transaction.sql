a5480b6d032b7c5fd6d2a9493db7383d065c68e6
Misty <mistyb140@gmail.com>
2025-06-23 18:13:12 -0700

    update create tables to latest schema

--- database/tables/transaction.sql
+++ database/tables/transaction.sql
@@ -4,11 +4,10 @@ GO
 CREATE TABLE Transactions (
     id UNIQUEIDENTIFIER PRIMARY KEY,
     user_id INT NOT NULL,
+    resident_id INT NOT NULL,
     transaction_type INT NOT NULL,
     transaction_date DATETIME DEFAULT GETDATE() NOT NULL,
     building_id INT,
-    unit_number NVARCHAR(10),
-    resident_name NVARCHAR(50),
 );
 
 GO

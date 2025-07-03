a442138250191f0699a96e92bdf523c091337642
Salik <contact@salikkhan.com>
2025-07-03 12:15:49 +0530

    feat(checkout): Allow exceeding category limits with warnings
    Allows users to add items over the category limit and proceed to the final checkout page.
    
    - A warning is now displayed when a category limit is exceeded.
    - The final checkout dialog will now show an error and block confirmation until the cart is within limits.
    - Backend validation remains unchanged to enforce limits.
    
    Fixes PIT-322

--- src/components/Checkout/ItemQuantityButton.tsx
+++ src/components/Checkout/ItemQuantityButton.tsx
@@ -1,53 +1,83 @@
-import { Remove, Add } from "@mui/icons-material";
-import { Box, Button, IconButton, Typography } from "@mui/material";
-import { CheckoutCardProps, CheckoutItemProp } from "../../types/interfaces";
+import { Remove, Add } from '@mui/icons-material';
+import { Box, Button, IconButton, Typography } from '@mui/material';
+import { CheckoutCardProps, CheckoutItemProp } from '../../types/interfaces';
 
-const ItemQuantityButton = ({ item, categoryCheckout, addItemToCart, removeItemFromCart, removeButton, disableAdd, categoryLimit, categoryName }: CheckoutCardProps) => {
-
+const ItemQuantityButton = ({
+  item,
+  categoryCheckout,
+  addItemToCart,
+  removeItemFromCart,
+  removeButton,
+  disableAdd,
+  categoryName,
+}: CheckoutCardProps) => {
   const foundInCart = categoryCheckout?.items?.find(
-    (v: CheckoutItemProp) => v.id === item.id
+    (v: CheckoutItemProp) => v.id === item.id,
   );
 
   const handleAddClick = () => {
-    if (categoryCheckout.categoryCount !== undefined && categoryCheckout.categoryCount < categoryLimit) {
-      addItemToCart(item, 1, categoryName);
-    } else {
-      return;
-    }
+    addItemToCart(item, 1, categoryName);
   };
 
   const handleRemoveClick = () => {
     addItemToCart(item, -1, categoryName);
-  }
+  };
 
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', whiteSpace: 'nowrap', overflow: 'hidden' }}>
-      {foundInCart ? <><IconButton
-        sx={{
-          backgroundColor: '#E8E8E8', width: { xs: '40px', lg: '30px' },
-          height: { xs: '40px', lg: '30px' }
-        }}
-        onClick={handleRemoveClick}
-      >
-        <Remove sx={{ fontSize: {xs: 'extra-large', lg: 'large' }}}/>
-      </IconButton>
-        <Typography
-          sx={{ fontSize: { xs: '14px', md: '20px' }, mx: { xs: '10px', md: '15px'} }}
-          data-testid="test-id-quantity"
-        >
-          {foundInCart.quantity}
-        </Typography></> : null}
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+      }}
+    >
+      {foundInCart ? (
+        <>
+          <IconButton
+            sx={{
+              backgroundColor: '#E8E8E8',
+              width: { xs: '40px', lg: '30px' },
+              height: { xs: '40px', lg: '30px' },
+            }}
+            onClick={handleRemoveClick}
+          >
+            <Remove sx={{ fontSize: { xs: 'extra-large', lg: 'large' } }} />
+          </IconButton>
+          <Typography
+            sx={{
+              fontSize: { xs: '14px', md: '20px' },
+              mx: { xs: '10px', md: '15px' },
+            }}
+            data-testid="test-id-quantity"
+          >
+            {foundInCart.quantity}
+          </Typography>
+        </>
+      ) : null}
       <IconButton
         sx={{
-          backgroundColor: '#E8E8E8', width: { xs: '40px', lg: '30px' },
-          height: { xs: '40px', lg: '30px' }
+          backgroundColor: '#E8E8E8',
+          width: { xs: '40px', lg: '30px' },
+          height: { xs: '40px', lg: '30px' },
         }}
         onClick={handleAddClick}
         disabled={disableAdd}
       >
-        <Add sx={{ fontSize: {xs: 'extra-large', lg: 'large' }}}/>
+        <Add sx={{ fontSize: { xs: 'extra-large', lg: 'large' } }} />
       </IconButton>
-      {removeButton ? <Button sx={{ color: 'black', fontSize: {xs: '14px', md: '16px'}, paddingX: { xs: '10px', md: '40px'}}} onClick={() => removeItemFromCart(item.id, categoryName)}>Remove</Button> : null}
+      {removeButton ? (
+        <Button
+          sx={{
+            color: 'black',
+            fontSize: { xs: '14px', md: '16px' },
+            paddingX: { xs: '10px', md: '40px' },
+          }}
+          onClick={() => removeItemFromCart(item.id, categoryName)}
+        >
+          Remove
+        </Button>
+      ) : null}
     </Box>
   );
 };

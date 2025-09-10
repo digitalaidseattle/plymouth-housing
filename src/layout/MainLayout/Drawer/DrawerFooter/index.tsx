ea4753a7b77d173474e1e039f7f33c0b211b8cde
Maarten <bospoort@live.com>
2025-09-10 15:19:36 -0700

    add version to UI

--- src/layout/MainLayout/Drawer/DrawerFooter/index.tsx
+++ src/layout/MainLayout/Drawer/DrawerFooter/index.tsx
@@ -4,13 +4,17 @@
  *  @copyright 2024 Digital Aid Seattle
  *
  */
+import { Box, Typography } from '@mui/material';
 
 // ==============================|| DRAWER HEADER ||============================== //
 
 const DrawerFooter = () => {
   return (
-      <>
-    </>
+    <Box sx={{ p: 2, textAlign: 'center' }}>
+      <Typography variant="caption" color="textSecondary">
+        Version: {APP_VERSION}
+      </Typography>
+    </Box>
   );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
+  define: {
+    'APP_VERSION': JSON.stringify(process.env.npm_package_version)
+  },
   plugins: [
     react(),
     visualizer({ open: false, filename: 'bundle-visualization.html' })

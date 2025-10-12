bc6c542af7e082f9176f36d673a1040be22a8636
Misty <mistyb140@gmail.com>
2025-10-12 13:18:55 -0700

    remove unneeded comments

--- database/procedures/process_inventory_reset_quantity.sql
+++ database/procedures/process_inventory_reset_quantity.sql
@@ -31,18 +31,14 @@ BEGIN
     BEGIN TRANSACTION
     
     BEGIN TRY            
-        -- First, create the transaction ID
         DECLARE @new_transaction_id UNIQUEIDENTIFIER;
 
-        -- Log to Transaction Table 
-        -- Transaction type of '3' to differentiate from a regular inventory update
         EXEC LogTransaction
             @user_id = @user_id,
-            @transaction_type = 3,
+            @transaction_type = 3, -- 3: TransactionTypes.CORRECTION
             @resident_id = NULL,
             @new_transaction_id = @new_transaction_id OUTPUT;
         
-        -- Log to Transaction Item Table
         BEGIN
             EXEC LogTransactionItem
                 @transaction_id = @new_transaction_id,

695e4d792017b17170239dd8305131f95d403780
maarten <bospoort@live.com>
2025-10-15 13:55:00 -0700

    update API path

---
+++ docs/DAB-setup.md
@@ -0,0 +1,13 @@
+Follow steps here:
+https://learn.microsoft.com/en-us/azure/data-api-builder/command-line/install
+
+Prerequisite is .NET 8 with aspnetcore runtime
+
+start dab
+dab start -c ./dab/dab.config.json
+
+test with swagger
+http://127.0.0.1:5000/swagger/index.html
+
+leave dab running. 
+start another terminal
--- src/types/constants.ts
+++ src/types/constants.ts
@@ -6,22 +6,22 @@ export const API_HEADERS = {
 
 export const ENDPOINTS = {
   //tables
-  USERS: '/data-api/rest/users',
-  ITEMS: '/data-api/rest/items',
-  CATEGORY: '/data-api/rest/categories',
-  BUILDINGS: '/data-api/rest/building',
-  UNITS: '/data-api/rest/units',
-  RESIDENTS: '/data-api/rest/residents',
+  USERS: '/data-api/api/users',
+  ITEMS: '/data-api/api/items',
+  CATEGORY: '/data-api/api/categories',
+  BUILDINGS: '/data-api/api/building',
+  UNITS: '/data-api/api/units',
+  RESIDENTS: '/data-api/api/residents',
   //Stored Procedures
-  VERIFY_PIN: '/data-api/rest/verify-pin',
-  CHECKOUT_GENERAL_ITEMS: '/data-api/rest/checkout-general-items',
-  CHECKOUT_WELCOME_BASKET: '/data-api/rest/checkout-welcome-basket',
-  PROCESS_INVENTORY_CHANGE: '/data-api/rest/process-inventory-change',
-  RECENT_TRANSACTIONS: '/data-api/rest/recent-transactions',
-  CHECK_PAST_CHECKOUT: '/data-api/rest/check-past-checkout',
+  VERIFY_PIN: '/data-api/api/verify-pin',
+  CHECKOUT_GENERAL_ITEMS: '/data-api/api/checkout-general-items',
+  CHECKOUT_WELCOME_BASKET: '/data-api/api/checkout-welcome-basket',
+  PROCESS_INVENTORY_CHANGE: '/data-api/api/process-inventory-change',
+  RECENT_TRANSACTIONS: '/data-api/api/recent-transactions',
+  CHECK_PAST_CHECKOUT: '/data-api/api/check-past-checkout',
   //Views
-  EXPANDED_ITEMS: '/data-api/rest/itemswithcategory',
-  CATEGORIZED_ITEMS: '/data-api/rest/itemsbycategory',
+  EXPANDED_ITEMS: '/data-api/api/itemswithcategory',
+  CATEGORIZED_ITEMS: '/data-api/api/itemsbycategory',
 } as const;
 
 export const SETTINGS = {

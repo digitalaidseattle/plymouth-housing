1932f04a96b114bcb83ef415968e363f9f055911
maarten <bospoort@live.com>
2025-10-27 11:09:03 -0700

    address PR comments

--- database/tests/test_inventory_change.sql
+++ database/tests/test_inventory_change.sql
@@ -10,7 +10,7 @@ where id = 2
 
 
 print 'Test add inventory'
-DECLARE @new_transaction_id UNIQUEIDENTIFIER;
+DECLARE @new_transaction_id UNIQUEIDENTIFIER = NEWID();
 exec ProcessInventoryChange @user_id = 1, @item = N'[
       {
         "id": 2,
@@ -27,23 +27,21 @@ from Items
 where id = 2
 
 print 'Test reduce inventory'
+SET @new_transaction_id = NEWID();
 exec ProcessInventoryChange @user_id = 1, @item = N'[
       {
-        "id": 2,
+        "id": 112,
         "quantity": -3
       }
     ]', @new_transaction_id = @new_transaction_id
 
-select *
-from Transactions
-select *
-from TransactionItems
 select name, quantity
 from Items
-where id = 2
+where id = 112
 
 
 print 'Test adjust inventory'
+SET @new_transaction_id = NEWID();
 exec ProcessInventoryResetQuantity @user_id = 1, @item_id = 111, @new_quantity = 12, @additional_notes = N'[
     {\"comments\":\"\",\"howYouKnow\":\"estimated\"}    
   ]'

e02a52917f1afdf5c7e07875766bc7adc10b6059
Misty <mistyb140@gmail.com>
2025-11-03 17:24:10 -0800

    stored procedure to get checkout history for a given date

---
+++ database/procedures/find_transaction_history.sql
@@ -0,0 +1,23 @@
+DROP PROCEDURE IF EXISTS [dbo].[FindCheckoutHistory];
+GO
+
+CREATE PROCEDURE FindCheckoutHistory
+    @checkout_date Date
+AS
+BEGIN
+    SELECT 
+        Transactions.id, 
+        user_id,  
+        TransactionItems.item_id, 
+        TransactionItems.quantity,
+        Residents.name, 
+        Units.unit_number,
+        Buildings.code
+    FROM Transactions
+    INNER JOIN TransactionItems ON Transactions.id = TransactionItems.transaction_id
+    INNER JOIN Residents ON Transactions.resident_id = Residents.id
+    INNER JOIN Units ON Residents.unit_id = Units.id
+    INNER JOIN Buildings ON Units.building_id = Buildings.id
+    WHERE CONVERT(date, [transaction_date]) = @checkout_date
+    AND [transaction_type] = 1;
+END;

e51195af3d2fc8e61855aef5d4b2b1da6c34e6e9
Misty <mistyb140@gmail.com>
2025-11-03 18:47:08 -0800

    include user's name in query

--- database/procedures/find_checkout_history.sql
+++ database/procedures/find_checkout_history.sql
@@ -8,6 +8,7 @@ BEGIN
     SELECT 
         Transactions.id, 
         user_id,  
+        Users.name,
         TransactionItems.item_id, 
         TransactionItems.quantity,
         Residents.name, 
@@ -18,6 +19,7 @@ BEGIN
     INNER JOIN Residents ON Transactions.resident_id = Residents.id
     INNER JOIN Units ON Residents.unit_id = Units.id
     INNER JOIN Buildings ON Units.building_id = Buildings.id
+    INNER JOIN Users ON Transactions.user_id = Users.id
     WHERE CONVERT(date, [transaction_date]) = @checkout_date
     AND [transaction_type] = 1;
 END;

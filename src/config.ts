bd434e21f46bcc5cbc670176c2a2bf1b8b1328fc
Mohamed Ibrahim <mohamed.ibrahim@digitalaidseattle.org>
2026-05-27 17:18:52 -0700

    increased main left drawer width to acommadate the bigger font size of the logo

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ const config = {
 };
 
 export default config;
-export const drawerWidth = 260;
+export const drawerWidth = 275;
 
 export const twitterColor = '#1DA1F2';
 export const facebookColor = '#3b5998';

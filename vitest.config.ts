a2184148e4facbc1a95b20a02399edf67b1cd0cb
Navami Bhat <127547240+Nav0708@users.noreply.github.com>
2026-08-05 15:04:21 -0700

    Remove setupFiles from Vitest configuration
    Removed setupFiles configuration from Vitest.

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/tests/setupTests.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
